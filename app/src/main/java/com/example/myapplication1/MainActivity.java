b891c1e777f9e4600bfc7aa5054e99469c5dc0cc
stepin104702 <69145013+stepin104702@users.noreply.github.com>
2020-11-09 17:32:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/myapplication1/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickHandler(View view) {
+        Toast.makeText(this, "Welcome Keerthana", Toast.LENGTH_SHORT).show();
+        EditText nameEditText;
+        nameEditText = findViewById(R.id.ed1);
+        String input=nameEditText.getText().toString();
+        TextView welcomeTextView;
+        welcomeTextView=findViewById(R.id.textView2);
+        welcomeTextView.setText(input);
+    }
+}
